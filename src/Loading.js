2b654625a7e63990edfd7bde2c394a5d2a183063
Duduling <duduling20@gmail.com>
2020-03-28 16:40:01 +0900

    feat: Add loading page

---
+++ src/Loading.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from '@emotion/native'
+
+// #region
+const Container = styled.View`
+    flex: 1;
+    justify-content: flex-end;
+    padding: 60px 32px;
+    background-color: #FDF6AA;
+`
+
+const LoadingText = styled.Text`
+    color: #2c2c2c;
+    font-size: 28px;
+`
+// #endregion
+
+const Loading = () => {
+    return (
+        <Container>
+            <LoadingText>Getting the weather...</LoadingText>
+        </Container>
+    )
+}
+
+export default Loading
