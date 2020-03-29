a32ac88d4aa41451cd04e2e703890a6b83e889da
Duduling <duduling20@gmail.com>
2020-03-29 15:02:37 +0900

    feat: Add Loading process

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import Loading from './Loading'
 
-export default function App() {
-  return <Loading />
+import * as Location from 'expo-location'
+import { Alert } from 'react-native'
+
+class App extends React.Component {
+  state = {
+    isLoading: true
+  }
+
+  getLoction = async () => {
+    try {
+      await Location.requestPermissionsAsync()
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync()
+      this.setState({ isLoading: false })
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad")
+    }
+  }
+
+  componentDidMount() {
+    this.getLoction()
+  }
+
+  render() {
+    const { isLoading } = this.state
+
+    return isLoading ? <Loading /> : null
+  }
+
 }
+
+export default App
